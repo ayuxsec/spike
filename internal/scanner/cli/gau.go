7f8966d7f1d96bf66741a12e28a54a3faca2cc17
ayuxsec <249073442+ayuxsec@users.noreply.github.com>
2025-12-17 18:17:27 +0530

    fix(gau): correctly handle domain wilcard arguments for subs in RunGau function

--- internal/scanner/cli/gau.go
+++ internal/scanner/cli/gau.go
@@ -4,6 +4,7 @@ import (
 	"spike/pkg/config"
 	"spike/pkg/logger"
 	"strconv"
+	"strings"
 )
 
 // gau passively fetches known URLs for the given targets from various sources
@@ -13,6 +14,11 @@ func RunGau(domain string, g *config.GauConfig) ([]string, error) {
 		logger.Warn("Gau is disabled in the config, skipping Gau scan")
 		return nil, nil
 	}
-	args := []string{"--threads", strconv.Itoa(g.Threads), "--subs", domain}
+	args := []string{"--threads", strconv.Itoa(g.Threads)}
+	if strings.HasPrefix(domain, "*.") {
+		args = append(args, "--subs", domain)
+	} else {
+		args = append(args, domain)
+	}
 	return RunCommand("gau", args)
 }
