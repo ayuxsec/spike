28524c807418f9199bb0ed2b0250467cac9cd68c
ayuxsec <249073442+ayuxsec@users.noreply.github.com>
2025-12-17 22:31:41 +0530

    refactor(scanner): clean  httpx

--- internal/scanner/cli/httpx.go
+++ internal/scanner/cli/httpx.go
@@ -8,16 +8,7 @@ import (
 )
 
 func RunHTTPX(targets []string, cfg *config.HTTPXConfig) ([]string, error) {
-	var targetType string
-	switch {
-	case strings.HasPrefix(targets[0], "http://"),
-		strings.HasPrefix(targets[0], "https://"):
-		targetType = "http servers"
-	default:
-		targetType = "(sub)domains"
-	}
-
-	logger.Infof("Running httpx on %d %s", len(targets), targetType)
+	logger.Infof("Running httpx on %d %s", len(targets), inferHttpxTargetType(targets[0]))
 
 	args := []string{"-threads", strconv.Itoa(cfg.Threads)}
 	if cfg.ScreenShot {
@@ -25,4 +16,14 @@ func RunHTTPX(targets []string, cfg *config.HTTPXConfig) ([]string, error) {
 	}
 
 	return RunCommandWithStdinInput("httpx", args, targets)
+}
+
+func inferHttpxTargetType(url string) string {
+	switch {
+	case strings.HasPrefix(url, "http://"),
+		strings.HasPrefix(url, "https://"):
+		return "http servers"
+	default:
+		return "(sub)domains"
+	}
 }
